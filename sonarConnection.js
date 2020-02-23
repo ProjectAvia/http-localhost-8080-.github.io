4668a5699399134198a6942d8f515afb8e1b6a6c
ProjectAvia <61375590+ProjectAvia@users.noreply.github.com>
2020-02-23 11:15:22 +0200

    Add files via upload

---
+++ sonarConnection.js
@@ -0,0 +1,28 @@
+﻿var sonarConnection = (function () {
+    'use strict';
+
+    var sonarHub;
+
+    var processSonarData = function (sonarData) {
+        
+        $.each(sonarData, function (index, item) {
+            sonarImage.draw(item.Angle, item.Distance);
+            sonarStats.fillTable(item.Angle, item.Distance);
+        });
+    };
+
+    return {
+        init: function (url) {
+            $.connection.hub.url = url;
+
+            sonarHub = $.connection.sonarHub;
+
+            if (sonarHub) {
+                sonarHub.client.sonarData = processSonarData;
+                $.connection.hub.start();
+            } else {
+                alert('Sonar hub not found!');
+            }
+        }
+    };
+}());
---
+++ sonarStats.js
@@ -0,0 +1,11 @@
+﻿var sonarStats = (function () {
+    'use strict';
+
+    return {       
+        fillTable: function (angle, distance ) {                     
+
+            $('#angle').text(angle);
+            $('#distance').text(distance);
+        }
+    };
+}());
